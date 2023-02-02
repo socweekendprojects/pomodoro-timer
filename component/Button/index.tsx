78df675c26a90781860c331081cef6591bf111fb
camillabrizzi <camillabrizzi@icloud.com>
2023-02-02 14:57:36 +0000

    completed basic play and reset functionality

--- component/Button/index.tsx
+++ component/Button/index.tsx
@@ -1,7 +1,12 @@
 import React from 'react'
+type ButtonProps = {
+  handleClick:  (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void
+  buttonText: string,
+}
 
-export default function Button() {
+
+export default function Button({handleClick, buttonText}:ButtonProps) {
   return (
-    <div>Button</div>
+    <button onClick={handleClick}>{buttonText}</button>
   )
 }
